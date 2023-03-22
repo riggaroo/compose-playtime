4600fff0f38b6fa51dd6d91ffc7b3bb371b07aec
riggaroo <riggaroo@google.com>
2023-03-22 19:54:45 +0000

    added drag to listen box

--- app/src/main/java/dev/riggaroo/composeplaytime/pager/DribbbleInspirationPager.kt
+++ app/src/main/java/dev/riggaroo/composeplaytime/pager/DribbbleInspirationPager.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.rememberSplineBasedDecay
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -32,6 +33,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.StarBorder
 import androidx.compose.material.icons.filled.StarRate
+import androidx.compose.material.icons.rounded.MusicNote
 import androidx.compose.material.icons.rounded.Star
 import androidx.compose.material.icons.rounded.StarBorder
 import androidx.compose.material3.Card
@@ -49,6 +51,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.lerp
@@ -61,6 +64,7 @@ import androidx.compose.ui.util.lerp
 import coil.compose.rememberAsyncImagePainter
 import dev.riggaroo.composeplaytime.rememberRandomSampleImageUrl
 import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
 
 /**
  * https://dribbble.com/shots/17117814--Drag-This-audio-player-prototype
@@ -69,24 +73,18 @@ import kotlin.math.absoluteValue
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun DribbbleInspirationPager() {
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(Color(0xFFECECEC))) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFECECEC))
+    ) {
         val pagerState = rememberPagerState()
         HorizontalPager(
             pageCount = 10,
             pageSpacing = 16.dp,
             beyondBoundsPageCount = 2,
             state = pagerState,
-            modifier = Modifier.fillMaxSize(),
-            flingBehavior = PagerDefaults.flingBehavior(state = pagerState,
-                lowVelocityAnimationSpec = spring(
-                    dampingRatio = Spring.DampingRatioHighBouncy,
-                    stiffness = Spring.StiffnessHigh),
-                snapAnimationSpec = spring(
-                    dampingRatio = Spring.DampingRatioMediumBouncy,
-                    stiffness = Spring.StiffnessMedium),
-                highVelocityAnimationSpec = rememberSplineBasedDecay())
+            modifier = Modifier.fillMaxSize()
         ) { page ->
             Box(modifier = Modifier.fillMaxSize()) {
                 SongInformationCard(
@@ -104,87 +102,122 @@ fun DribbbleInspirationPager() {
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun SongInformationCard(modifier: Modifier = Modifier, pagerState: PagerState, page: Int) {
-    var dragAmount by remember {
-        mutableStateOf(0f)
-    }
-    val draggableState = rememberDraggableState(onDelta = { dragAmount += it})
-    Card(modifier = modifier
-        .draggable(draggableState, orientation = Orientation.Vertical)
-        .graphicsLayer {
-            translationY = dragAmount
-        }
-        .fillMaxWidth()
-        .wrapContentHeight()
-        .shadow(16.dp, ambientColor = Color.LightGray),
+fun SongInformationCard(
+    pagerState: PagerState,
+    page: Int,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .shadow(16.dp, ambientColor = Color.LightGray),
         shape = RoundedCornerShape(32.dp),
         colors = elevatedCardColors(containerColor = Color.White)
-        ) {
-        Column(modifier = Modifier.padding(32.dp)) {
-            val pageOffset = ((pagerState.currentPage - page) + pagerState
+    ) {
+        Column(modifier = Modifier) {
+            val pageOffset = 1 - ((pagerState.currentPage - page) + pagerState
                 .currentPageOffsetFraction).absoluteValue
-            Log.d("TAG", "pageoffset $pageOffset, $page")
             Image(
                 modifier = Modifier
+                    .padding(32.dp)
                     .clip(RoundedCornerShape(24.dp))
                     .aspectRatio(1f)
                     .background(Color.LightGray)
                     .graphicsLayer {
-                        val lerpedOffset = lerp(1f, 1.75f, 1 - pageOffset)
-                        scaleX *= lerpedOffset
-                        scaleY *= lerpedOffset
+                        // get a scale value between 1 and 1.75f, 1.75 will be when its resting,
+                        // 1f is the smallest it'll be when not the focused page
+                        val scale = lerp(1f, 1.75f, pageOffset)
+                        // apply the scale equally to both X and Y, to not distort the image
+                        scaleX *= scale
+                        scaleY *= scale
                     },
                 painter = rememberAsyncImagePainter(
                     model = rememberRandomSampleImageUrl
                         (width = 1200)
                 ),
                 contentScale = ContentScale.Crop,
-                contentDescription = "Song Image"
+                contentDescription = null
             )
-            Spacer(modifier = Modifier.padding(8.dp))
-
-            Text("Artist",
-                fontSize = 16.sp,
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center)
-            Spacer(modifier = Modifier.padding(4.dp))
-            Text("A Song Title",
-                fontSize = 22.sp,
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center)
-            Spacer(modifier = Modifier.padding(8.dp))
-            StarRating(3)
-
-            Log.d("tag","currentPageOffset $pageOffset")
-            Column(){
-                Spacer(modifier = Modifier
-                    .height(100.dp * (1 - pageOffset))
-                    .fillMaxWidth())
+            SongDetails()
+            Box(
+                modifier = Modifier
+                    .height(150.dp * pageOffset)
+                    .fillMaxWidth()
+                    .graphicsLayer {
+                        alpha = pageOffset
+                    }
+            ) {
+                Column(modifier = Modifier.align(Alignment.BottomCenter), horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(Icons.Rounded.MusicNote, contentDescription = "",
+                        modifier = Modifier.padding(8.dp).size(36.dp))
+                    Text("DRAG TO LISTEN")
+                    Spacer(modifier = Modifier.size(4.dp))
+                    Canvas(modifier = Modifier.padding(4.dp).fillMaxWidth().height(60.dp)) {
+                        val sizeGap = 16.dp.toPx()
+                        val numberDotsHorizontal = size.width / sizeGap
+                        val numberDotsVertical = size.height / sizeGap
+                        repeat(numberDotsHorizontal.roundToInt()) { horizontal ->
+                            repeat (numberDotsVertical.roundToInt()) { vertical ->
+                                drawCircle(Color.LightGray, radius = 2.dp.toPx(), center = Offset(horizontal * sizeGap, vertical * sizeGap))
+                            }
+                        }
+                    }
+                }
             }
         }
     }
 }
 
 @Composable
-fun StarRating(stars: Int, modifier: Modifier = Modifier) {
-    Row(modifier = modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center) {
-        for (i in 0 until stars) {
-            Icon(Icons.Rounded.Star,
-                contentDescription = "Star",
-                tint = Color(0xFFFF9800),
-                modifier = Modifier.size(36.dp)
-                )
-        }
-        for (i in 0 until (5-stars)){
-            Icon(Icons.Rounded.StarBorder, contentDescription = "Star empty",
-                modifier = Modifier.size(36.dp)
-                )
-        }
-    }
+private fun SongDetails() {
+    Spacer(modifier = Modifier.padding(8.dp))
+    Text(
+        "Artist",
+        fontSize = 16.sp,
+        modifier = Modifier.fillMaxWidth(),
+        textAlign = TextAlign.Center
+    )
+    Spacer(modifier = Modifier.padding(4.dp))
+    Text(
+        "A Song Title",
+        fontSize = 24.sp,
+        modifier = Modifier.fillMaxWidth(),
+        textAlign = TextAlign.Center
+    )
+    Spacer(modifier = Modifier.padding(8.dp))
+    StarRating(3)
 }
 
 @Composable
-fun DragToListen() {
-
+fun StarRating(stars: Int, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        for (i in 0 until stars) {
+            Icon(
+                Icons.Rounded.Star,
+                contentDescription = "Star",
+                tint = Color(0xFFFF9800),
+                modifier = Modifier.size(36.dp)
+            )
+        }
+        for (i in 0 until (5 - stars)) {
+            Icon(
+                Icons.Rounded.StarBorder, contentDescription = "Star empty",
+                modifier = Modifier.size(36.dp)
+            )
+        }
+    }
 }
+/*
+
+@Composable
+fun DragToListen() {
+    var dragAmount by remember {
+        mutableStateOf(0f)
+    }
+    val draggableState = rememberDraggableState(onDelta = { dragAmount += it })
+
+}*/

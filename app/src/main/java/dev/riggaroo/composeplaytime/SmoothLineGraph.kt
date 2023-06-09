a70d28fe3a7dce0c156ef7b1640ec18982f56ede
Jolanda Verhoef <JolandaVerhoef@users.noreply.github.com>
2023-06-09 16:27:36 +0200

    Add gesture to graph

--- app/src/main/java/dev/riggaroo/composeplaytime/SmoothLineGraph.kt
+++ app/src/main/java/dev/riggaroo/composeplaytime/SmoothLineGraph.kt
@@ -2,41 +2,56 @@ package dev.riggaroo.composeplaytime
 
 import android.graphics.PointF
 import android.os.Build
+import android.view.HapticFeedbackConstants
 import androidx.annotation.RequiresApi
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.clipRect
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toIntRect
+import androidx.compose.ui.unit.toSize
 import kotlinx.coroutines.launch
 import java.math.BigDecimal
 import java.time.LocalDate
 import kotlin.math.roundToInt
 
 /*
-* Copyright 2022 The Android Open Source Project
+* Copyright 2023 The Android Open Source Project
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -50,6 +65,7 @@ import kotlin.math.roundToInt
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
+@OptIn(ExperimentalTextApi::class)
 @RequiresApi(Build.VERSION_CODES.O)
 @Preview
 @Composable
@@ -59,25 +75,49 @@ fun SmoothLineGraph() {
             .background(PurpleBackgroundColor)
             .fillMaxSize()
     ) {
-        val animationProgress = remember {
-            Animatable(0f)
+        val animationProgress = remember { Animatable(0f) }
+        var highlightedWeek by remember { mutableStateOf(-1) }
+        val localView = LocalView.current
+
+        LaunchedEffect(highlightedWeek) {
+            if (highlightedWeek != -1) {
+                localView.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS)
+            }
         }
 
         LaunchedEffect(key1 = graphData, block = {
             animationProgress.animateTo(1f, tween(3000))
         })
+
         val coroutineScope = rememberCoroutineScope()
+        val textMeasurer = rememberTextMeasurer()
+        val labelTextStyle = MaterialTheme.typography.labelSmall
+
         Spacer(
             modifier = Modifier
                 .padding(8.dp)
                 .aspectRatio(3 / 2f)
                 .fillMaxSize()
                 .align(Alignment.Center)
-                .clickable {
-                    coroutineScope.launch {
-                        animationProgress.snapTo(0f)
-                        animationProgress.animateTo(1f, tween(3000))
+                .pointerInput(Unit) {
+                    detectTapGestures {
+                        coroutineScope.launch {
+                            animationProgress.snapTo(0f)
+                            animationProgress.animateTo(1f, tween(3000))
+                        }
                     }
+                }
+                .pointerInput(Unit) {
+                    detectDragGesturesAfterLongPress(
+                        onDragStart = { offset ->
+                            highlightedWeek = (offset.x / (size.width / (graphData.size - 1))).roundToInt()
+                        },
+                        onDragEnd = { highlightedWeek = -1 },
+                        onDragCancel = { highlightedWeek = -1 },
+                        onDrag = { change, _ ->
+                            highlightedWeek = (change.position.x / (size.width / (graphData.size - 1))).roundToInt()
+                        }
+                    )
                 }
                 .drawWithCache {
                     val path = generateSmoothPath(graphData, size)
@@ -127,6 +167,48 @@ fun SmoothLineGraph() {
                                     )
                                 ),
                                 style = Fill
+                            )
+                        }
+
+                        // draw highlight if user is dragging
+                        if (highlightedWeek != -1) {
+                            val amount = graphData[highlightedWeek].amount
+                            val minAmount = graphData.minBy { it.amount }.amount
+                            val range = graphData.maxBy { it.amount }.amount - minAmount
+                            val percentageHeight = ((amount - minAmount).toFloat() / range.toFloat())
+                            val pointY = size.height - (size.height * percentageHeight)
+                            // draw vertical line on week
+                            val x = highlightedWeek * (size.width / (graphData.size - 1))
+                            drawLine(
+                                HighlightColor,
+                                start = Offset(x, 0f),
+                                end = Offset(x, size.height),
+                                strokeWidth = 2.dp.toPx(),
+                                pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f))
+                            )
+
+                            // draw hit circle on graph
+                            drawCircle(
+                                Color.Green,
+                                radius = 4.dp.toPx(),
+                                center = Offset(x, pointY)
+                            )
+
+                            // draw info box
+                            val textLayoutResult = textMeasurer.measure("$amount", style = labelTextStyle)
+                            val highlightContainerSize = (textLayoutResult.size).toIntRect().inflate(4.dp.roundToPx()).size
+                            val boxTopLeft = ( x - (highlightContainerSize.width / 2f))
+                                .coerceIn(0f, size.width - highlightContainerSize.width)
+                            drawRoundRect(
+                                Color.White,
+                                topLeft = Offset(boxTopLeft, 0f),
+                                size = highlightContainerSize.toSize(),
+                                cornerRadius = CornerRadius(8.dp.toPx())
+                            )
+                            drawText(
+                                textLayoutResult,
+                                color = Color.Black,
+                                topLeft = Offset(boxTopLeft + 4.dp.toPx(), 4.dp.toPx())
                             )
                         }
                     }
@@ -223,7 +305,7 @@ data class Balance(val date: LocalDate, val amount: BigDecimal)
 
 val PurpleBackgroundColor = Color(0xff322049)
 val BarColor = Color.White.copy(alpha = 0.3f)
-
+val HighlightColor = Color.White.copy(alpha = 0.7f)
 
 @Preview
 @Composable
@@ -256,5 +338,5 @@ fun CoordinateSystem(){
             )
         }
 
-    })
+        })
 }
